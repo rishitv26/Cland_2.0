320e1dd60f4e4ec63cabda2730a54232b20ca4ad
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-02-04 16:03:14 -0500

    done with register python, going to work on static

--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('login/', include('login.urls')),
+    path('create-account/', include('register.urls'))
 ]
