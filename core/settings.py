56e05f1ae46ca4e837e7fb16649cc8c5eda8f375
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-02-07 12:02:44 -0500

    creating all the pannels needed for staff

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'home',
     'login',
     'register',
+    'pannels',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('db-admin/', admin.site.urls),
     path('', include('home.urls')),
     path('login/', include('login.urls')),
-    path('create-account/', include('register.urls'))
+    path('create-account/', include('register.urls')),
+    path('pannels/', include('pannels.urls')),
 ]
