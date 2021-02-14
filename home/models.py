23563d6929947df6557d3341f3026543204876b4
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-02-14 14:30:52 -0500

    thega

--- home/models.py
+++ home/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 
-class Conversation(models.Model):
-    title = models.CharField(max_length=64)
-    pic = models.CharField(max_length=2000)
-    bg_pic = models.CharField(max_length=2000)
