b204192d32109065b47321ff8fcf6d1ac335adf3
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-02-18 13:36:26 -0500

    xgfdxtr x

--- home/views.py
+++ home/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 def index(request):
 <<<<<<< HEAD
+<<<<<<< HEAD
     return render(request, "home.html")
     
+=======
+    return render(request, "home.html")
+>>>>>>> parent of b2de6d0... sdgvye
 =======
     return render(request, "home.html")
 >>>>>>> parent of b2de6d0... sdgvye
