4387acfac0f7b990cd5dd75b6073452f5bba0592
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-03-06 19:58:55 -0500

    Delete home directory

--- home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from corporations.models import *
-
-def index(request):
-    con = Corporation.objects.all()
-    return render(request, "home.html", {
-        'cons': con
-    })
