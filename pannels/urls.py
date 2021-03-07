513545e840b355b72f5b1254bb3ca57eb1c7269b
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-03-06 19:59:16 -0500

    Delete pannels directory

--- pannels/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/', views.adminPage, name="advanced administration"),
-    path('mail/', views.emailPage, name='email'),
-]
